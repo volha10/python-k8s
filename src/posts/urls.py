fb184282b421ddc1b49cf1f233b1bde620274499
Volha Dziatko <volha10.dziatko@gmail.com>
2024-08-11 17:58:47 +0200

    add minimal home page

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "posts"
+
 urlpatterns = [
     path('', views.index, name="index"),
-    path('<int:pk>/', views.DetailView.as_view(), name="detail")
+    path('<int:pk>/', views.DetailView.as_view(), name="detail"),
 ]
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views import generic
 
+from .forms import PostForm
 from .models import Post
 
 
 def index(request):
-    return render(request, "posts/index.html")
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            post = form.save()
+            return redirect(reverse('posts:detail', args=[post.id]))
+        else:
+            print("Something went wrong.")
+
+    else:
+        form = PostForm()
+        posts = Post.objects.all()
+
+        return render(request, "posts/index.html", {"form": form, "posts": posts})
 
 
 class DetailView(generic.DetailView):
